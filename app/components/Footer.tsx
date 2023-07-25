24756e4352e80b1768f0b6e11440e0d17a95999d
VHall1 <victorhallpsn@hotmail.com>
2023-07-25 03:45:23 +0100

    stub new footer

--- app/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Footer: React.FC = () => {
-  return (
-    <footer style={{ display: "flex" }} id="footer">
-      <p>Made with &#x2764; by Victor Hall</p>
-    </footer>
-  );
-};
